f56aaf9d10e793de0221192aa781efab33d54e2b
gowthaman <igowthamanks@gmail.com>
2023-05-30 15:29:58 +0530

    code splitting implementation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { Suspense } from 'react';
 import './App.css';
-import HomePage from './components/homePage';
-import NavBar from './components/navBar';
-import LocationPage from './components/locationPage';
+import { CircularProgress } from '@mui/material';
+
+const HomePage  = React.lazy(()=>import('./components/homePage'));
+const NavBar = React.lazy(()=>import('./components/navBar'));
+const LocationPage = React.lazy(()=>import('./components/locationPage'));
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<NavBar/>}>
-            <Route index element={<HomePage location/>}/>
-            <Route path='/:locationid' element={<LocationPage/>}/>
-          </Route>
-        </Routes>
+        <Suspense fallback={<h1><CircularProgress/></h1>}>
+          <Routes>
+            <Route path='/' element={<NavBar/>}>
+              <Route index element={<HomePage location/>}/>
+              <Route path='/:locationid' element={<LocationPage/>}/>
+            </Route>
+          </Routes>
+        </Suspense>
+        
       </BrowserRouter>
     </div>
   );
